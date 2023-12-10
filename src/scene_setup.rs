7ca48ca8ac7ed08d4afa51de2a3b0999a3f5091a
Benjamin Cates <thebenjicat@gmail.com>
2023-12-09 20:34:54 -0800

    Refactor scene setup

--- src/scene_setup.rs
+++ src/scene_setup.rs
@@ -40,19 +40,34 @@ impl WallBundle {
         };
     }
 }
-#[derive(Component)]
-pub struct PreviewLine(());
 
-/// Creates a scene with boundaries and a base
-/// The inner width of the box is 8 units, each wall is 0.5 units thick
-/// The height of the box is 12 units
 pub(crate) fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     ball_templates: Res<balls::BallTemplates>,
 ) {
-    let mat = materials.add(StandardMaterial {
+    spawn_box(&mut commands, &mut materials, &mut meshes);
+    spawn_lights(&mut commands);
+    commands.spawn(camera::new_camera());
+    let mut example_ball = balls::Ball::new(1);
+    example_ball.spatial.transform.translation = Vec3::new(0.0, 12.0, 0.0);
+    example_ball
+        .spawn(&ball_templates, &mut commands)
+        .insert(balls::ExampleBall(()))
+        .insert(GravityScale(0.0))
+        .insert(RigidBody::Kinematic);
+}
+
+/// Creates a ball-holding box with walls and a base
+/// The inner width of the box is 8 units, each wall is 0.5 units thick
+/// The height of the box is 12 units
+fn spawn_box(
+    commands: &mut Commands,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    meshes: &mut ResMut<Assets<Mesh>>,
+) {
+    let wall_mat = materials.add(StandardMaterial {
         alpha_mode: AlphaMode::Blend,
         base_color: Color::rgba(0.3, 0.3, 0.3, 0.12),
         double_sided: true,
@@ -69,7 +84,7 @@ pub(crate) fn setup(
             meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
                 BOX_WIDTH, BOX_HEIGHT,
             )))),
-            mat.clone(),
+            wall_mat.clone(),
             Quat::from_rotation_y(side * PI / 2.0),
         ));
     }
@@ -82,12 +97,12 @@ pub(crate) fn setup(
             meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
                 BOX_WIDTH, BOX_HEIGHT,
             )))),
-            mat.clone(),
+            wall_mat.clone(),
             Quat::from_rotation_y((side - 1.0) * PI / 2.0),
         ));
     }
-    // Base box
-    let mat = materials.add(StandardMaterial {
+    // Base of the box
+    let base_mat = materials.add(StandardMaterial {
         alpha_mode: AlphaMode::Blend,
         base_color: Color::rgba(1.0, 0.4, 1.0, 1.0),
         double_sided: true,
@@ -101,42 +116,11 @@ pub(crate) fn setup(
         meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
             BOX_WIDTH, BOX_WIDTH,
         )))),
-        mat.clone(),
+        base_mat.clone(),
         Quat::from_rotation_x(PI / 2.0),
     ));
 
-    //Lighting
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            illuminance: 4000.0,
-            shadows_enabled: true,
-            color: Color::rgb(1.0, 1.0, 0.8),
-            ..default()
-        },
-        transform: Transform {
-            rotation: Quat::from_rotation_y(1.2) * Quat::from_rotation_x(-0.5),
-            ..default()
-        },
-        ..default()
-    });
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            illuminance: 8000.0,
-            shadows_enabled: true,
-            color: Color::rgb(1.0, 1.0, 0.8),
-            ..default()
-        },
-        transform: Transform {
-            rotation: Quat::from_rotation_y(2.6) * Quat::from_rotation_x(-0.8),
-            ..default()
-        },
-        ..default()
-    });
-    commands.insert_resource(AmbientLight {
-        color: Color::WHITE,
-        brightness: 0.5,
-    });
-    commands.spawn(camera::new_camera());
+    // Black lines representing the edges of the box
     let line_mat = materials.add(StandardMaterial {
         base_color: Color::BLACK,
         unlit: true,
@@ -170,11 +154,38 @@ pub(crate) fn setup(
         material: line_mat,
         ..default()
     });
-    let mut example_ball = balls::Ball::new(1);
-    example_ball.spatial.transform.translation = Vec3::new(0.0, 12.0, 0.0);
-    example_ball
-        .spawn(&ball_templates, &mut commands)
-        .insert(balls::ExampleBall(()))
-        .insert(GravityScale(0.0))
-        .insert(RigidBody::Kinematic);
+}
+
+// Spawn two direcitonal lights and an ambient light
+fn spawn_lights(commands: &mut Commands) {
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 4000.0,
+            shadows_enabled: true,
+            color: Color::rgb(1.0, 1.0, 0.8),
+            ..default()
+        },
+        transform: Transform {
+            rotation: Quat::from_rotation_y(1.2) * Quat::from_rotation_x(-0.5),
+            ..default()
+        },
+        ..default()
+    });
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 8000.0,
+            shadows_enabled: true,
+            color: Color::rgb(1.0, 1.0, 0.8),
+            ..default()
+        },
+        transform: Transform {
+            rotation: Quat::from_rotation_y(2.6) * Quat::from_rotation_x(-0.8),
+            ..default()
+        },
+        ..default()
+    });
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.5,
+    });
 }
