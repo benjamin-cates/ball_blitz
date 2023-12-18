ce5cfad3cc239bcfa03da5464c911b3aabbb428d
Benjamin Cates <thebenjicat@gmail.com>
2023-12-18 12:32:43 -0800

    Update texture of base

--- src/setup.rs
+++ src/setup.rs
@@ -142,9 +142,8 @@ fn spawn_box(
     // Base of the box
     let base_mat = materials.add(StandardMaterial {
         base_color: Color::rgba(0.1, 0.4, 0.2, 1.0),
-        metallic: 0.6,
-        perceptual_roughness: 0.3,
         double_sided: true,
+        reflectance: 0.1,
         cull_mode: None,
         ..default()
     });
